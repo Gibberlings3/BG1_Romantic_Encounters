7ba0952ae46033e121b52c37535eb96a1bbb8539
Gitjas <Gitjas@users.noreply.github.com>
2020-08-14 16:31:46 +0200

    "Scar's Return" compatibility
    -"Scar's Return": compatibility added with EBG1's Duke Eltan component
    -"Scar's Return": compatibility added with Transitions' Duke Eltan components
    -quest can be finished in SoD

---
+++ bg1re/eltan/c#eltan_no_ebg1.d
@@ -0,0 +1,15 @@
+APPEND %tutu_var%%ELTANDLG_HARBORMASTER%
+IF WEIGHT #-1
+~Dead("Sarevok")
+//Name("C#RE1ELT",Myself) //only if bg1re Duke Eltan is talking
+GlobalLT("C#RE1_ScarRetrieval","GLOBAL",16)~ intro_scarsreturn_after_sarevok
+SAY @322
++ ~PartyHasItem("c#re1sr5")~ + @1 /* ~I have here the body of Scar! I found him.~ */ DO ~TakePartyItem("c#re1sr5")~ + eltan_06_as
++ ~PartyHasItem("c#re1sr6")~ + @2 /* ~I have news about the body of Scar! I located it.~ */ DO ~TakePartyItem("c#re1sr6")~ + eltan_06_receit_as
++ ~Global("C#RE1_ScarRetrieval","GLOBAL",10)~ + @3 /* ~Did you get news about Scar's body yet?~ */ DO ~SetGlobal("C#RE1_ScarRetrieval","GLOBAL",12)~ + heard_scar_as
++ ~Global("C#RE1_ScarRetrieval","GLOBAL",11)~ + @4 /* ~What is Scar's status?~ */ DO ~SetGlobal("C#RE1_ScarRetrieval","GLOBAL",12)~ + burying_scar_as
++ ~Global("C#RE1_ScarRetrieval","GLOBAL",12)~ + @5 /*  ~Any news about the possible resurrection of Scar?~ */ + rising_scar_as
+++ ~Good day to you.~ EXIT
+END
+
+END //APPEND
---
+++ bg1re/eltan/c#eltan_sod_add.d
@@ -0,0 +1,62 @@
+/* addition to Scar's Return quest so it can be solved in SoD, too */
+
+APPEND BDELTAN
+
+/* Scar's return, Ducal Palace */
+IF WEIGHT #-1
+~AreaCheck("bd0102")
+GlobalGT("C#RE1_ScarRetrieval","GLOBAL",9)
+GlobalLT("C#RE1_ScarRetrieval","GLOBAL",13)~ THEN scar_is_here_sod
+SAY @9 /* ~Ah, <CHARNAME>, there you are. You will be very surprised to see who has returned, in the truest meaning there is. Make yourself ready to meet an old friend. I know he would like to thank you.~ */
+IF ~~ THEN DO ~SetGlobal("C#RE1_ScarRetrieval","GLOBAL",13)
+ClearAllActions()
+      StartCutSceneMode()	
+	CutSceneId(Player1)    
+        CreateCreature("c#re1sr6",[444.491]%FACE_6%) 
+ActionOverride("c#re1sr6",DestroyItem("%tutu_var%sw1h02"))
+ActionOverride("c#re1sr6",DestroyItem("%tutu_var%shld04"))
+ActionOverride(Player1,Face(1))
+EndcutSceneMode()~ EXIT
+END
+
+IF WEIGHT #-1
+~AreaCheck("bd0102")
+Global("C#RE1_ScarRetrieval","GLOBAL",14)~ THEN scar_was_here_sod
+SAY @315
+IF ~~ THEN DO ~SetGlobal("C#RE1_ScarRetrieval","GLOBAL",16)~ EXIT
+END
+
+IF WEIGHT #-1
+~AreaCheck("bd0108") Global("C#RE1_ScarRetrieval","GLOBAL",15)~ THEN scar_was_attacked_sod
+SAY @316
+IF ~~ THEN DO ~SetGlobal("C#RE1_ScarRetrieval","GLOBAL",16)~ EXIT
+END
+
+END //APPEND
+
+/* 
+Global("C#RE1_ScarRetrieval","GLOBAL",9): PC got either receit or Scar's body
+quest is active but PC cannot give Scar's body for ~GlobalGT("C#RE1_ScarRetrieval","GLOBAL",0) 
+GlobalLT("C#RE1_ScarRetrieval","GLOBAL",9)~
+*/
+
+INTERJECT BDELTAN 10 c#lc_eltan_sod
+== BDELTAN IF ~GlobalGT("C#RE1_ScarRetrieval","GLOBAL",0) 
+GlobalLT("C#RE1_ScarRetrieval","GLOBAL",9)~ THEN @323 /* ~First, though, let me tell you that we finally found Scar. There were loyal servants who hid his body before Angelo and his minions could let it vanish for good.~ */
+= @318 /* ~We set the body to a well-earned rest, young friend. It's the only thing we could do. It was a decent burial, albeit somewhat hushed and not appropriate for the Second in Command of the Flaming Fist who served for so many years.~ */
+= @317 /* ~I promise you, loyal friend, that I will do anything in my power for my former Second in Command. If there is a way to raise him, I will have him raised.~ */
+END
+IF ~~ THEN DO ~EraseJournalEntry(@53)
+EraseJournalEntry(@59)
+EraseJournalEntry(@74)
+EraseJournalEntry(@86)
+EraseJournalEntry(@114)
+EraseJournalEntry(@134)
+EraseJournalEntry(@137)
+EraseJournalEntry(@145)
+EraseJournalEntry(@166)
+EraseJournalEntry(@198)
+EraseJournalEntry(@301)
+EraseJournalEntry(@309)
+EraseJournalEntry(@10000)~ SOLVED_JOURNAL @324 EXIT
+
